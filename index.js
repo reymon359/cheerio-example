59807f6cb8996a33a351f08f44b8aadcdc5bf1d7
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-13 07:07:02 +0200

    getting the title

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const URL = 'https://www.imdb.com/title/tt0319061/';
 
 (async() => {
 
+    const response = await request(URL);
+
+    let $ = cheerio.load(response);
+
+    let title = $('div[class="title_wrapper"] > h1').text();
+
+    console.log(title);
 })()
