2566b712c4fc525703a1555caafc052e2e33996b
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-14 19:48:01 +0200

    scraping 2 sites

--- index.js
+++ index.js
@@ -1,45 +1,61 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
 
-const URL = 'https://www.imdb.com/title/tt0319061/';
+const URLS = [
+    'https://www.imdb.com/title/tt0319061/',
+    'https://www.imdb.com/title/tt0167260/'
+];
 
 (async() => {
 
-    const response = await request({
-        uri: URL,
-        headers: {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'en,es;q=0.9,es-ES;q=0.8',
-            'cache-control': 'max-age=0',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-        },
-        gzip: true // To uncompress responses
+    let moviesData = [];
 
-    });
+    for (let movie of URLS) {
 
-    let $ = cheerio.load(response);
+        const response = await request({
+            uri: movie,
+            headers: {
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en,es;q=0.9,es-ES;q=0.8',
+                'cache-control': 'max-age=0',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'none',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+            },
+            gzip: true // To uncompress responses
 
-    let title = $('div[class="title_wrapper"] > h1').text().trim();
-    let rating = $('span[itemprop="ratingValue"]').text();
-    let poster = $('div[class="poster"] > a > img').attr('src');
-    let totalRatings = $('div[class="imdbRating"] > a').text();
-    let releaseDate = $('a[title="See more release dates"]').text().trim();
-    let genres = [];
-    $('div[class="title_wrapper"] a[href^="/search/"]').each((i, elm) => {
-        let genre = $(elm).text();
-        genres.push(genre);
-    });
+        });
+
+        let $ = cheerio.load(response);
+
+        let title = $('div[class="title_wrapper"] > h1').text().trim();
+        let rating = $('span[itemprop="ratingValue"]').text();
+        let poster = $('div[class="poster"] > a > img').attr('src');
+        let totalRatings = $('div[class="imdbRating"] > a').text();
+        let releaseDate = $('a[title="See more release dates"]').text().trim();
+        let genres = [];
+
+        $('div[class="title_wrapper"] a[href^="/search/"]').each((i, elm) => {
+            let genre = $(elm).text();
+            genres.push(genre);
+        });
+
+        moviesData.push({
+            title,
+            rating,
+            poster,
+            totalRatings,
+            releaseDate,
+            genres
+        })
 
 
-    console.log(`title: ${title}`);
-    console.log(`rating: ${rating}`);
-    console.log(`poster: ${poster}`);
-    console.log(`totalRatings: ${totalRatings}`);
-    console.log(`releaseDate: ${releaseDate}`);
-    console.log(`genres: ${genres}`);
+
+    }
+
+    console.log(moviesData);
+    debugger;
 })()
