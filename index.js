e7ac31116d67d0a7d267de86342d956425da5029
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-14 21:14:13 +0200

    downloading images

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const request = require('request-promise');
+const requestPromise = require('request-promise');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const { Parser } = require('json2csv');
+const request = require('request');
 
-const URLS = [
-    'https://www.imdb.com/title/tt0319061/',
-    'https://www.imdb.com/title/tt0167260/'
+const URLS = [{
+        url: 'https://www.imdb.com/title/tt0319061/',
+        id: 'big_fish'
+    },
+    // {
+    //     url: 'https://www.imdb.com/title/tt0167260/',
+    //     id: 'lord_of_the_rings_3'
+    // }
 ];
 
 (async() => {
@@ -14,8 +20,8 @@ const URLS = [
 
     for (let movie of URLS) {
 
-        const response = await request({
-            uri: movie,
+        const response = await requestPromise({
+            uri: movie.url,
             headers: {
                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
                 'accept-encoding': 'gzip, deflate, br',
@@ -54,18 +60,41 @@ const URLS = [
             releaseDate,
             genres
         })
+
+        // Downloading files (images)
+        let file = fs.createWriteStream(`${movie.id}.jpg`);
+
+        let stream = request({
+            uri: poster,
+            headers: {
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en,es;q=0.9,es-ES;q=0.8',
+                'cache-control': 'max-age=0',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'none',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+            },
+            gzip: true
+        }).pipe(file);
     }
 
 
+
+
+
+
+    /* To save the data as Json or Csv
     const fields = ['title', 'rating'];
 
     const json2csvParser = new Parser();
     const csv = json2csvParser.parse(moviesData);
 
     // fs.writeFileSync('./data.json', JSON.stringify(moviesData), 'utf-8');
-    fs.writeFileSync('./data.csv', csv, 'utf-8');
+    fs.writeFileSync('./data.csv', csv, 'utf-8');*/
 
 
-    console.log(csv);
 
 })()
